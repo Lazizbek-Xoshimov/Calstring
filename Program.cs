ecbbaaa0aa88da8b8011ab9078f2a3a68899472f
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-03 21:02:39 +0500

    2 ta operand uchun Calstring ishlab chiqildi

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string action = Console.ReadLine();
+
+int firstNumber = action[0] - '0';
+int secondNumber = action[0] - '0';
+
+switch (action[1])
+{
+    case '+':
+        {
+            Console.WriteLine(firstNumber + secondNumber);
+            break;
+        }
+    case '-':
+        {
+            Console.WriteLine(firstNumber - secondNumber);
+            break;
+        }
+}
