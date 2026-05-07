015c72db45b8828e1ccb918b21f1301c0165fb83
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-07 11:44:36 +0500

    Trash code olib tashlandi, traditional structure ga o'tkazildi

--- Program.cs
+++ Program.cs
@@ -1,60 +1,60 @@
-﻿string inputUser = string.Empty;
-do
+﻿using System;
+
+namespace Calstring
 {
-    Console.WriteLine("Hisoblamoqchi bo'lgan qo'shish va ayirish amallarini kiriting.");
-    Console.Write("(Izoh. Faqat a+b-c-d... ketma-ketligida kiriting): ");
-
-    string action = Console.ReadLine();
-
-    int count = 0;
-    int summa = 0;
-    string number = "0";
-    char sign = '+';
-
-    while (count != action.Length)
+    public class Program
     {
-        if (action[count] == '+' || action[count] == '-')
+        public static void Main(string[] args)
         {
-            switch (sign)
+            string inputUser = string.Empty;
+            do
             {
-                case '+':
+                Console.WriteLine("Hisoblamoqchi bo'lgan qo'shish va ayirish amallarini kiriting.");
+                Console.Write("(Izoh. Faqat a+b-c-d... ketma-ketligida kiriting): ");
+
+                string action = Console.ReadLine();
+
+                int count = 0;
+                int summa = 0;
+                string number = "0";
+                char sign = '+';
+
+                while (count <= action.Length)
+                {
+                    if (count == action.Length || action[count] == '+' || action[count] == '-')
                     {
-                        summa += int.Parse(number);
-                        break;
+                        switch (sign)
+                        {
+                            case '+':
+                                {
+                                    summa += int.Parse(number);
+                                    break;
+                                }
+                            case '-':
+                                {
+                                    summa -= int.Parse(number);
+                                    break;
+                                }
+                        }
+
+                        if (count < action.Length)
+                            sign = action[count];
+
+                        number = "";
                     }
-                case '-':
+                    else
                     {
-                        summa -= int.Parse(number);
-                        break;
+                        number += action[count];
                     }
-            }
-            number = "";
-            sign = action[count];
+
+                    count ++;
+                }
+
+                Console.WriteLine(action + "=" + summa);
+                Console.WriteLine("Dasturdan chiqishni xohlaysizmi?");
+                Console.Write("(ha/yo'q): ");
+                inputUser = Console.ReadLine();
+            } while (inputUser.ToLower() == "yo'q");
         }
-        else
-        {
-            number += action[count];
-        }
-
-        count ++;
     }
-
-    switch (sign)
-    {
-        case '+':
-            {
-                summa += int.Parse(number);
-                break;
-            }
-        case '-':
-            {
-                summa -= int.Parse(number);
-                break;
-            }    
-    }
-
-    Console.WriteLine(action + "=" + summa);
-    Console.WriteLine("Dasturdan chiqishni xohlaysizmi?");
-    Console.Write("(ha/yo'q): ");
-    inputUser = Console.ReadLine();
-} while (inputUser.ToLower() == "yo'q");
+}
